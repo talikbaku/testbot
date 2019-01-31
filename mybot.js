e5cd8095ee882bc6271724dc70009dadec4b9476
talikbaku <33751259+talikbaku@users.noreply.github.com>
2019-02-01 02:10:10 +0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,10 +1,11 @@
-	const TelegramBot = require('node-telegram-bot-api');
+const TeleBot = require('telebot');
+const bot = new TeleBot('766944180:AAHzp2kclClMfnyi0J5lej2ttNGTNQJ5GwM');
 
-	const token = '766944180:AAHzp2kclClMfnyi0J5lej2ttNGTNQJ5GwM';
-	const bot = new TelegramBot(token, {polling: true})
+// On every text message
+bot.on('text', msg => {
+    let id = msg.from.id;
+    let text = msg.text;
+    return bot.sendMessage(id, `You said: ${ text }`);
+});
 
-	bot.on('message', msg => {
-		bot.sendMessage(msg.chat.id, `hello baby, bot say: "hi, ${msg.from.first_name}`)
-	})
-
-	
+bot.connect();
