9082588e50b333e3dd0da0b7f699a6bf7301407b
talikbaku <33751259+talikbaku@users.noreply.github.com>
2019-02-01 01:54:13 +0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,26 +1,10 @@
-	var TelegramBot = require('node-telegram-bot-api');
+	const TelegramBot = require('node-telegram-bot-api');
 
-	var token = '766944180:AAHzp2kclClMfnyi0J5lej2ttNGTNQJ5GwM';
-	var bot = new TelegramBot(token, {polling: true});
+	const token = '766944180:AAHzp2kclClMfnyi0J5lej2ttNGTNQJ5GwM';
+	const bot = new TelegramBot(token, {polling: true})
 
-	var notes = [];
+	bot.on('message', msg => {
+		bot.sendMessage(msg.chat.id, `hello baby, bot say: "hi, ${msg.from.first_name}`)
+	})
 
-	bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
-	  var userId = msg.from.id;
-	  var text = match[1];
-	  var time = match[2];
-
-	  notes.push( { 'uid':userId, 'time':time, 'text':text } );
-
-	  bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-	});
-
-	setInterval(function(){
-		for (var i = 0; i < notes.length; i++){
-			var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-				if ( notes[i]['time'] == curDate ) {
-					bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-					notes.splice(i,1);
-				}
-			}
-	},1000);
+	
