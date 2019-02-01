6a3c68faa9b488a9076c9fc05211797406e3aa67
talikbaku <33751259+talikbaku@users.noreply.github.com>
2019-02-02 03:16:06 +0400

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -1,5 +1,14 @@
-const TeleBot = require('telebot');
-const bot = new TeleBot('766944180:AAHzp2kclClMfnyi0J5lej2ttNGTNQJ5GwM');
+	// const TelegramBot = require('node-telegram-bot-api');
+
+	// const token = '763881402:AAEdrcyIU_PgcM18_X9rP1v3MN0A5VViY1o';
+	// const bot = new TelegramBot(token, {polling: true})
+
+	// bot.on('message', msg => {
+	// 	bot.sendMessage(msg.chat.id, `hello baby, bot say: "hi, ${msg.from.first_name}`)
+	// })
+
+	const TeleBot = require('telebot');
+const bot = new TeleBot('763881402:AAEdrcyIU_PgcM18_X9rP1v3MN0A5VViY1o');
 
 // On every text message
 bot.on('text', msg => {
